b06db2fccebe903c89a911bf67831424cd9190fb
Tuva Björnberg <tuva.bjornberg02@gmail.com>
2024-11-05 13:18:27 +0100

    formatted

--- assignment1/task1.py
+++ assignment1/task1.py
@@ -4,12 +4,12 @@ from scipy import signal
 import sounddevice as sd
 import time
 
-# Task 1 
+# Task 1
 
 # x1
-T1 = 0.01 
+T1 = 0.01
 
-#x2
+# x2
 f2 = 1000
 
 interval = [0, 5]
@@ -19,21 +19,21 @@ dt = 0.00001
 
 t = np.arange(interval[0], interval[1], dt)
 
-x1 = A * np.sin((1 / T1) * 2 * np.pi * t) # f = 1/T, w= 2pif
+x1 = A * np.sin((1 / T1) * 2 * np.pi * t)  # f = 1/T, w= 2pif
 x2 = A * np.sin(f2 * 2 * np.pi * t)
 
 fig, ax = plt.subplots()
 
 ax.title.set_text("Plot of the sinusoid signals x1 and x2")
-ax.plot(t, x1, label='x1')
-ax.plot(t, x2, label='x2')
-ax.set_xlabel('t (s)')
-ax.set_ylabel('Amplitude')
+ax.plot(t, x1, label="x1")
+ax.plot(t, x2, label="x2")
+ax.set_xlabel("t (s)")
+ax.set_ylabel("Amplitude")
 ax.legend()
 ax.set_xlim([0, 0.01])
 ax.set_ylim([-1, 1])
 plt.show()
 
-sd.play(x2, 1/dt, blocking=True)
+sd.play(x2, 1 / dt, blocking=True)
 time.sleep(1)
 sd.stop()
