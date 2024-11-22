e4aa979d134bfaf66b9aff20c18f62133f10e47a
Tuva Björnberg <tuva.bjornberg02@gmail.com>
2024-11-22 21:24:18 +0100

    final touches task1

--- assignment2/task1.py
+++ assignment2/task1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 T0 = 5e-3
 omega_0 = 2 * np.pi / T0
 
@@ -9,15 +8,14 @@ n = np.arange(-12, 12, dtype=float)
 omega_interval = n * omega_0
 
 c_n = np.zeros_like(n, dtype=complex)
-
 non_zero = n[n != 0]
 c_n[n != 0] = (-1) ** non_zero * 1j / (non_zero * np.pi)
 
 X_w = 2 * np.pi * c_n
 
 magnitude = np.abs(X_w)
-
 phase = []
+# Phase unwrapping
 for w in range(len(n)):
     angle = np.angle(X_w[w])
 
@@ -28,23 +26,17 @@ for w in range(len(n)):
     else:
         phase.append(angle)
 
-
-# phase = np.angle(X_w)
-
 fig, axs = plt.subplots(1, 2)
-fig.suptitle(
-    "Plot represents the magnitude and phase shift of the frequency response H(s)"
-)
+fig.suptitle("Plot represents the magnitude and phase shift of X($\omega$)")
 
-axs[0].stem(omega_interval, magnitude, basefmt=" ")
-axs[0].set_title("Magnitude Spectrum of $X(s)$")
+axs[0].stem(omega_interval, magnitude, basefmt=" ", markerfmt="^")
+axs[0].set_title("Magnitude Spectrum of X($\omega$)")
 axs[0].set(xlabel=r"$\omega$ (rad/s)", ylabel=r"$|X(\omega)|$")
 axs[0].grid()
 
 axs[1].stem(omega_interval, phase, basefmt=" ")
-axs[1].set_title("Phase Spectrum of $X(s)$")
+axs[1].set_title("Phase Spectrum of X($\omega$)")
 axs[1].set(xlabel=r"$\omega$ (rad/s)", ylabel=r"$\angle X(\omega)$ (rad)")
 axs[1].grid()
 
-# fig.legend()
 plt.show()
