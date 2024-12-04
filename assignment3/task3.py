79c0852a8864d52cb3dccb59dd53173f915a8328
Tuva Björnberg <tuva.bjornberg02@gmail.com>
2024-12-04 12:57:08 +0100

    task 3, recoder and playback

--- assignment3/task3.py
+++ assignment3/task3.py
@@ -8,17 +8,14 @@ t = [0, 10]
 
 fs = 40e3
 Ts = 1 / fs
-
 K = t[1] * fs
 
 x = sd.rec(int(K), fs, channels=1, blocking=True)
 
-sd.play(x, fs, blocking=True)
+sd.play(x, fs, blocking=True)  # Original call, 40kHz sampling rate
 
 print(x.shape)
-
 x = x[::5]  # pick every 5th sample in the recording, --> 8kHz
-
 print(x.shape)
 
-sd.play(x, 8e3, blocking=True)
+sd.play(x, 8e3, blocking=True)  # Resampled call, 8kHz sampling rate
